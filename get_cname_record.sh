a6d196e5f715c11695a3dfbe51225824cba585f7
ThoundsN <inthebybyby@gmail.com>
2019-07-27 10:26:51 +0800

    small changes

--- get_cname_record.sh
+++ get_cname_record.sh
@@ -7,8 +7,8 @@ grepcname(){
       mkdir -p "$basepath/cname/$(basename $dir)"
       latestfile=$(ls -t $1/*simple* | head -1 )
       echo "debug:  latestfile    $latestfile"
-      newfile="$basepath/cname/cname_$(basename $latestfile)/"
-      newfileparsed="""$basepath/cname/parsed_cname_$(basename $latestfile)/"
+      newfile="$basepath/cname/cname_$(basename $latestfile)"
+      newfileparsed="""$basepath/cname/parsed_cname_$(basename $latestfile)"
       echo "debug: $newfile"
       echo "debug: $newfileparsed"
       touch "$newfile"
