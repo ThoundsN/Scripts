0452e5524d5c6e08763c22ab95f873007a745b8f
ThoundsN <inthebybyby@gmail.com>
2019-07-31 15:18:34 +0800

    small changes

---
+++ remove_false_positive_massdns.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+import sys
+
+filename = sys.argv[1]
+
+dns_dict = dict()
+
+count = 12
+
+with open(filename,encoding='utf-8') as input:
+    for row in input:
+        domain, recordtype, resource = row.split()
+        if resource in dns_dict:
+            if len(dns_dict[resource[0]])> count:
+                continue
+            dns_dict[resource[0]].append(domain)
+        else:
+            dns_dict[resource] = [[domain], recordtype]
+
+for k,list in dns_dict.items():
+    for row in list:
+        for entity in row[0]:
+            print(' '.join(map(str,[entity,row[1],k])))
+
