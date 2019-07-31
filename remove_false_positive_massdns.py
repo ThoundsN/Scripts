8fb3ef4bdd66c6979788d1c4c25341494a5a0afa
ThoundsN <inthebybyby@gmail.com>
2019-07-31 15:25:18 +0800

    small changes

--- remove_false_positive_massdns.py
+++ remove_false_positive_massdns.py
@@ -10,9 +10,13 @@ count = 12
 
 with open(filename,encoding='utf-8') as input:
     for row in input:
+        print(dns_dict)
         domain, recordtype, resource = row.split()
+        print(domain)
+        print(recordtype)
+        print(resource)
         if resource in dns_dict:
-            if len(dns_dict[resource[0]])> count:
+            if len(dns_dict[resource[0]]) > count:
                 continue
             dns_dict[resource[0]].append(domain)
         else:
@@ -22,4 +26,3 @@ for k,list in dns_dict.items():
     for row in list:
         for entity in row[0]:
             print(' '.join(map(str,[entity,row[1],k])))
-
