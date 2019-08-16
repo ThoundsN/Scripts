444d4ced6522dd6b3eb812bdaf03caef19314aac
ThoundsN <inthebybyby@gmail.com>
2019-08-16 19:05:00 +0800

    usual

---
+++ python/random_domain.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import random
+import string
+
+def randomString(stringLength=10):
+    """Generate a random string of fixed length """
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(stringLength))
+
+with open("Generated_domain.txt", 'w', encoding='utf-8') as w :
+    for i in range(100000):
+        w.write(randomString(5)+'.dev.jupiterone.io')
+        w.write('\n')
+
+w.close()
