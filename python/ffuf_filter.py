aff740daa6fcea8a98e28a367432733cfa59406a
ThoundsN <inthebybyby@gmail.com>
2019-09-30 15:04:25 +0800

    fj

--- python/ffuf_filter.py
+++ python/ffuf_filter.py
@@ -5,35 +5,35 @@ filename = sys.argv[1]
 outputfilename = "filtered_"+filename
 
 
-def processing_line(d,line, count=500):
+def processing_line(d,line, count=10):
     delimeter = ","
     size = line.split(delimeter)[1].split(":")[1].strip()
     size = int(size)
     if size in d :
-        if d[size].length() < count:
+        if len(d[size])< count:
             d[size].append(line)
     else:
         d[size] = [line]
 
-def print_dict(d,output, count=500):
+def print_dict(d,output, count=10):
     for k,v in d.items():
-        if v.length() < count:
+        if len(v) < count:
             for line in v:
                 output.write(line)
 
 
 with open(outputfilename, 'w', encoding='utf-8') as output:
     with open(filename, 'r', encoding='utf-8') as input:
-        lines = input.readline()
+        lines = input.readlines()
         for line in lines:
-            if line.startswith("        /'___\  /'___\ "):
-                try:
-                    print_dict(d,output)
-                except NameError:
-                    d = dict()
-                output.write(line)
+            if  line.strip():
+                if line.startswith("        /'___"):
+                    try:
+                        print_dict(d,output)
+                    except NameError:
+                        d = dict()
 
-            if line[:1].isalpha():
-                processing_line(d,line)
-            else:
-                output.write(line)
+                if line[:2].isalpha() or line.startswith('[2K'):
+                    processing_line(d,line)
+                else:
+                    output.write(line)
