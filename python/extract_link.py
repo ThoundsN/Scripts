9d1ac0b71a561d2231f60d55f7822faa037c2515
ThoundsN <inthebybyby@gmail.com>
2020-02-15 11:42:46 +0800

    dsd

--- python/extract_link.py
+++ python/extract_link.py
@@ -4,27 +4,29 @@ import re
 from bs4 import BeautifulSoup
 
 input_path = sys.argv[1]
-output_path1=sys.argv[2]
-output_path2=sys.argv[3]
+output_path1 = sys.argv[2]
+output_path2 = sys.argv[3]
 
-specific_words="admin,test,dev,beta,internal,prod"
+specific_words = "admin,test,dev,beta,internal,prod"
 
-specific_words = re.split(',',specific_words)
+specific_words = re.split(',', specific_words)
 
 api_wordlist = "api v1 v2 v3 v4 v5 v6"
 
 api_wordlist = re.split(' ', api_wordlist)
 
-
 if __name__ == '__main__':
-    fp = open(input_path,'r',encoding="utf-8")
-    soup1 = BeautifulSoup(fp,"lxml")
-    for div in soup1.body.find_all('div',recursive=False):
+    fp = open(input_path, 'r', encoding="utf-8")
+    soup1 = BeautifulSoup(fp, "html.parser")
+    for div in soup1.body.find_all('div', recursive=False):
         # print(div)
         for word in specific_words:
-            if word not in str(div.a.string):
-                div.decompose()
-    fw1 = open(output_path1,'w',encoding="utf-8")
+            try:
+                if word not in str(div.a.string):
+                    div.decompose()
+            except:
+                continue
+    fw1 = open(output_path1, 'w', encoding="utf-8")
     fw1.write(soup1.prettify())
     fw1.close()
     fp.close()
@@ -32,14 +34,17 @@ if __name__ == '__main__':
     soup1.decompose()
     gc.collect()
 
-    fp = open(input_path,'r',encoding="utf-8")
-    soup2 = BeautifulSoup(fp,"lxml")
-    for div in soup2.body.find_all('div',recursive=False):
+    fp = open(input_path, 'r', encoding="utf-8")
+    soup2 = BeautifulSoup(fp, "html.parser")
+    for div in soup2.body.find_all('div', recursive=False):
         # print(div)
         for word in api_wordlist:
-            if word not in str(div.a.string):
-                div.decompose()
-    fw2 = open(output_path2,'w',encoding="utf-8")
+            try:
+                if word not in str(div.a.string):
+                    div.decompose()
+            except:
+                continue
+    fw2 = open(output_path2, 'w', encoding="utf-8")
     fw2.write(soup2.prettify())
     fw2.close()
     fp.close()
