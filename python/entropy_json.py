28302cc543e1ab96e77545c08e915cab259e9571
ThoundsN <inthebybyby@gmail.com>
2020-02-16 12:55:03 +0800

    sdd

--- python/entropy_json.py
+++ python/entropy_json.py
@@ -4,10 +4,21 @@ import sys
 input_path = sys.argv[1]
 output_path = sys.argv[2]
 
+def location2href(location):
+    prefix = "http://jsrecon.ragnarokv.site/links/"
+    pieces = location.split('/')
+    del pieces[6]
+    del pieces[0:5]
+    # print(pieces)
+    suffix = "/".join(pieces)
+    href = prefix + suffix
+    # print(href)
+    return  href
+
 fr = open(input_path,'r',encoding='utf-8')
 data = json.load(fr)
 
-print(type(data))
+# print(type(data))
 
 entropys = set()
 newdata = list()
@@ -20,8 +31,14 @@ for entry in data:
 
 # print(type(newdata))
 
-x = json.dumps(newdata)
 # print(type(x))
+
+for entry in newdata:
+    file_location = entry['File']
+    href = location2href(file_location)
+    entry['href'] = href
+
+x = json.dumps(newdata)
 
 fw = open(output_path,'w',encoding='utf-8')
 fw.write(x)
